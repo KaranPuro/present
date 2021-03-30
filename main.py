e3ada7942f776604d94152481dcf4f0bde8b6c67
KaranPuro <78454341+KaranPuro@users.noreply.github.com>
2021-03-30 16:27:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+
+for i in range(5):
+    for colors in ["red", "magenta", "blue", "cyan", "green", "yellow", "white"]:
+        turtle.color(colors)
+        turtle.pensize(3)
+        turtle.left(12)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
+        turtle.forward(200)
+        turtle.left(90)
